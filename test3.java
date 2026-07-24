99fff38b8768b8aaf0eb68aed36fb343878aa88e
Ashrita Behera <230375206+25cse001ashritabehera-gif@users.noreply.github.com>
2026-07-24 20:13:34 +0530

    Implement palindrome number checker
    Add palindrome check for a number in test3.java

---
+++ test3.java
@@ -0,0 +1,23 @@
+class test3
+  {
+    public static void main(String args[])
+    {
+      int num = Integer.parseInt(args[0]);
+      int temp = 0;
+      int rev = 0;
+      while(num != 0)
+        {
+          int d = num%10;
+          rev = rev*10+d;
+          num = num/10;
+        }
+      if(temp == rev)
+      {
+        System.out.println(temp +"is a palindrome number")
+      }
+      else
+      {
+        System.out.println(temp +"is not a palindrome number")
+      }
+    }
+  }
