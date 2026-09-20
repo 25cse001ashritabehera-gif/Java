8b50f8a6c9423f088153a2d66b9276819056512c
Ashrita Behera <230375206+25cse001ashritabehera-gif@users.noreply.github.com>
2026-09-20 12:55:52 +0530

    Implement box class with volume calculation
    Added a box class with a parameterized constructor and dimensions.

--- test6.java
+++ test6.java
@@ -1,3 +1,8 @@
+//##EXPERIMENT - 3 (CLASS, OBJECT, STATIC METHOD, CONSTRUCTOR)
+//#Q1. Create aa class box that uses a parameterized constructor to initialize the dimensions of a box.
+//The dimensions of the box are width, height, and depth. The class should have a method that can return the volume of the box. 
+//Create an object of the box class and test the functionalities.
+
 import java.util.*;
 class box
   {
